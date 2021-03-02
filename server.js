6bebb90ba5ae1a8668b7d47126e083a07d6e965a
BrandonVA <codebva@gmail.com>
2021-03-02 13:52:12 -0800

    Cleaned up code.

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-// // const express = require("express");
-// const path = require("path");
-// // const app = express();
-// // // const http = require("http").createServer(app);
-// // const io = require("socket.io")(app);
-
-// const express = require("express");
-// const app = express();
-// const http = require("http").Server(app);
-// const io = require("socket.io")(http);
-
-// app.use(express.static("public"));
-
-// app.get("/", (req, res) => {
-//   res.send(path.join(__dirname + "/index.html"));
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-// });
-
-// app.listen(3000, () => {
-//   console.log("listening on http://localhost:3000/");
-// });
-// const app = require("express")();
-
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -35,13 +9,9 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send(path.join(__dirname + "/index.html"));
-  //   res.sendFile(__dirname + "/index.html");
 });
 
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-// });
-
+// Console.logs when a user is connected.
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("disconnect", () => {
@@ -49,13 +19,14 @@ io.on("connection", (socket) => {
   });
 });
 
-// This will emit the event to all connected sockets
+// This will emit the event to all connected sockets supposed to but doesn't work??? -look into
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
   });
 });
 
+// doesn't work?
 io.emit("some event", {
   someProperty: "some value",
   otherProperty: "other value",
@@ -65,6 +36,7 @@ io.emit("some event", {
 //   socket.broadcast.emit("hi");
 // });
 
+// sends the sends/console.logs message
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);

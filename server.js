a4893e19280dbfb7586d7de8a0a5a1bf08fc1487
BrandonVA <codebva@gmail.com>
2021-03-04 13:55:35 -0800

    Set up when a user connects to a room it routes them to a room in socket.io

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
 
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -12,25 +20,45 @@ const PORT = 3000 || process.env.PORT;
 // serve static folder public
 app.use(express.static("public"));
 
+const adminName = "AI";
+
 // Run when a client connects
 io.on("connection", (socket) => {
-  console.log("New WS Connection");
+  // Listens for when a user joins the room
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-  // welcomes the user
-  socket.emit("message", "Welcome to Chat cord");
+    // joins a user to their room
+    socket.join(user.room);
 
-  // Broadcst when a user connects
-  socket.broadcast.emit("message", "A user has joined the chat");
+    // welcomes the user
+    socket.emit("message", formatMessage(adminName, "Welcome to Chat cord"));
 
-  // Runs when a Client disconnects
-  socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(adminName, `${user.username} has joined the chat`)
+      );
   });
 
   // listen for chatMessage
   socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
     console.log(msg);
-    io.emit("message", msg);
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when a Client disconnects
+  socket.on("disconnect", () => {
+    const user = getCurrentUser(socket.id);
+    userLeave(socket.id);
+
+    io.to(user.room).emit(
+      "message",
+      formatMessage(adminName, `${user.username} has left the chat`)
+    );
   });
 });
 

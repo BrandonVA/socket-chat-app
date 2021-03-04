966c121ac57f784c5b08ab31c46d1363214fdca4
BrandonVA <codebva@gmail.com>
2021-03-04 13:54:51 -0800

    updated output message to add data from the user posting the message like name and time

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,42 @@
 const chatForm = document.getElementById("chat-form");
-// const chatMessage;
+const chatMessages = document.querySelector(".chat-messages");
+
+// Get username and room from url
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+console.log(username, room);
 
 const socket = io();
 
+// Join chatroom
+socket.emit("joinRoom", { username, room });
+
 socket.on("message", (message) => {
   console.log(message);
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const msg = e.target.elements.msg.value;
+
+  const input = e.target.elements.msg;
+  const msg = input.value;
   console.log(msg);
 
   // emit a msg to the server
   socket.emit("chatMessage", msg);
+  input.value = "";
+  input.focus();
 });
+
+// output message to DOM
+const outputMessage = (message) => {
+  const { time, text, username } = message;
+
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">${username} <span>${time}</span></p> <p class="text">${text}</p>`;
+  document.querySelector(".chat-messages").append(div);
+};

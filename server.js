4899952faf9714331dfcc0726c1dab402de4b8df
BrandonVA <codebva@gmail.com>
2021-03-08 13:32:17 -0800

    making the socket io connection shorter for testing and redoing how the server is listening.

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const server = require("http").Server(app); // http.createServer(app);
+// const server = app.listen(PORT);
+const io = require("socket.io")(server);
+// const socketio = require("socket.io");
+
+// const PORT = process.env.PORT || 3001;
+
 const path = require("path");
-const http = require("http");
-const socketio = require("socket.io");
+// const http = require("http");
+// const socketio = require("socket.io");
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// app.listen(PORT, function () {
+//   console.log(`🌎 ==> API server now on port ${PORT}!`);
+// });
+
+// const server = http.createServer(app);
+// const server = app.listen(PORT);
+
+// const io = socketio(server);
 
 const formatMessage = require("./utils/messages");
 const {
@@ -11,71 +37,106 @@ const {
   getRoomUsers,
 } = require("./utils/users");
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+// // serve static folder public
+// app.use(express.static("public"));
 
-const PORT = 3000 || process.env.PORT;
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// serve static folder public
-app.use(express.static("public"));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-const adminName = "AI";
+// const adminName = "Socket Bot";
 
-// Run when a client connects
-io.on("connection", (socket) => {
-  // Listens for when a user joins the room
-  socket.on("joinRoom", ({ username, room }) => {
-    const user = userJoin(socket.id, username, room);
+// let interval;
+// // Run when a client connects
+// io.on("connection", (socket) => {
+//   console.log("connected to socket io");
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+//   interval = setInterval(() => getApiAndEmit(socket), 1000);
 
-    // joins a user to their room
-    socket.join(user.room);
+//   // Listens for when a user joins the room
+//   socket.on("joinRoom", ({ username, room }) => {
+//     const user = userJoin(socket.id, username, room);
+//     console.log("user joined");
 
-    // welcomes the user
-    socket.emit("message", formatMessage(adminName, "Welcome to Chat cord"));
+//     // joins a user to their room
+//     socket.join(user.room);
 
-    // Broadcast when a user connects
-    socket.broadcast
-      .to(user.room)
-      .emit(
-        "message",
-        formatMessage(adminName, `${user.username} has joined the chat`)
-      );
-    //send user and room info
-    io.to(user.room).emit("roomUsers", {
-      room: user.room,
-      users: getRoomUsers(user.room),
-    });
-  });
+//     // welcomes the user
+//     socket.emit("message", formatMessage(adminName, "Welcome to Chat cord"));
 
-  // listen for chatMessage
-  socket.on("chatMessage", (msg) => {
-    const user = getCurrentUser(socket.id);
-    io.to(user.room).emit("message", formatMessage(user.username, msg));
-  });
+//     // Broadcast when a user connects
+//     socket.broadcast
+//       .to(user.room)
+//       .emit(
+//         "message",
+//         formatMessage(adminName, `${user.username} has joined the chat`)
+//       );
+//     //send user and room info
+//     io.to(user.room).emit("roomUsers", {
+//       room: user.room,
+//       users: getRoomUsers(user.room),
+//     });
+//   });
 
-  // Runs when a Client disconnects
+//   // listen for chatMessage
+//   socket.on("chatMessage", (msg) => {
+//     const user = getCurrentUser(socket.id);
+//     console.log(msg);
+//     io.to(user.room).emit("message", formatMessage(user.username, msg));
+//   });
+
+//   // Runs when a Client disconnects
+//   socket.on("disconnect", () => {
+//     const user = getCurrentUser(socket.id);
+//     userLeave(socket.id);
+//     clearInterval(interval);
+
+//     if (user) {
+//       // notify user when another user leaves
+//       io.to(user.room).emit(
+//         "message",
+//         formatMessage(adminName, `${user.username} has left the chat`)
+//       );
+//       // update room and users
+//       io.to(user.room).emit("roomUsers", {
+//         room: user.room,
+//         users: getRoomUsers(user.room),
+//       });
+//     }
+//   });
+// });
+
+let interval;
+
+io.sockets.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
   socket.on("disconnect", () => {
-    const user = getCurrentUser(socket.id);
-    userLeave(socket.id);
-
-    if (user) {
-      // notify user when another user leaves
-      io.to(user.room).emit(
-        "message",
-        formatMessage(adminName, `${user.username} has left the chat`)
-      );
-      // update room and users
-      io.to(user.room).emit("roomUsers", {
-        room: user.room,
-        users: getRoomUsers(user.room),
-      });
-    }
+    console.log("Client disconnected");
+    clearInterval(interval);
   });
 });
 
-app.get("/", (req, res) => {
-  res.send(path.join(__dirname + "/index.html"));
-});
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+// app.get("/", (req, res) => {
+//   res.send(path.join(__dirname + "/index.html"));
+// });
+
+// server.listen(4000, () => console.log("Server running on port: 4000"));
 
 server.listen(PORT, () => console.log("Server running on port: " + PORT));

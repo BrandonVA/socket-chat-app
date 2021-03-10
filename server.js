9ad82fcf2093bc4427e27bd0d8b86e41603bd0f5
BrandonVA <codebva@gmail.com>
2021-03-10 13:49:20 -0800

    setting up server to use the socket io routes

--- server.js
+++ server.js
@@ -49,89 +49,83 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// const adminName = "Socket Bot";
+const adminName = "Socket Bot";
+
+// Run when a client connects
+io.on("connection", (socket) => {
+  console.log("connected to socket io");
+
+  // Listens for when a user joins the room
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    console.log("user joined");
+
+    // joins a user to their room
+    socket.join(user.room);
+
+    // welcomes the user
+    socket.emit("message", formatMessage(adminName, "Welcome to Chat cord"));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(adminName, `${user.username} has joined the chat`)
+      );
+    //send user and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+    console.log(msg);
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when a Client disconnects
+  socket.on("disconnect", () => {
+    const user = getCurrentUser(socket.id);
+    userLeave(socket.id);
+
+    if (user) {
+      // notify user when another user leaves
+      io.to(user.room).emit(
+        "message",
+        formatMessage(adminName, `${user.username} has left the chat`)
+      );
+      // update room and users
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
 
 // let interval;
-// // Run when a client connects
-// io.on("connection", (socket) => {
-//   console.log("connected to socket io");
+
+// io.sockets.on("connection", (socket) => {
+//   console.log("New client connected");
 //   if (interval) {
 //     clearInterval(interval);
 //   }
 //   interval = setInterval(() => getApiAndEmit(socket), 1000);
-
-//   // Listens for when a user joins the room
-//   socket.on("joinRoom", ({ username, room }) => {
-//     const user = userJoin(socket.id, username, room);
-//     console.log("user joined");
-
-//     // joins a user to their room
-//     socket.join(user.room);
-
-//     // welcomes the user
-//     socket.emit("message", formatMessage(adminName, "Welcome to Chat cord"));
-
-//     // Broadcast when a user connects
-//     socket.broadcast
-//       .to(user.room)
-//       .emit(
-//         "message",
-//         formatMessage(adminName, `${user.username} has joined the chat`)
-//       );
-//     //send user and room info
-//     io.to(user.room).emit("roomUsers", {
-//       room: user.room,
-//       users: getRoomUsers(user.room),
-//     });
-//   });
-
-//   // listen for chatMessage
-//   socket.on("chatMessage", (msg) => {
-//     const user = getCurrentUser(socket.id);
-//     console.log(msg);
-//     io.to(user.room).emit("message", formatMessage(user.username, msg));
-//   });
-
-//   // Runs when a Client disconnects
 //   socket.on("disconnect", () => {
-//     const user = getCurrentUser(socket.id);
-//     userLeave(socket.id);
+//     console.log("Client disconnected");
 //     clearInterval(interval);
-
-//     if (user) {
-//       // notify user when another user leaves
-//       io.to(user.room).emit(
-//         "message",
-//         formatMessage(adminName, `${user.username} has left the chat`)
-//       );
-//       // update room and users
-//       io.to(user.room).emit("roomUsers", {
-//         room: user.room,
-//         users: getRoomUsers(user.room),
-//       });
-//     }
 //   });
 // });
 
-let interval;
-
-io.sockets.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
-});
-
-const getApiAndEmit = (socket) => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
+// const getApiAndEmit = (socket) => {
+//   const response = new Date();
+//   // Emitting a new message. Will be consumed by the client
+//   socket.emit("FromAPI", response);
+// };
 
 // app.get("/", (req, res) => {
 //   res.send(path.join(__dirname + "/index.html"));
